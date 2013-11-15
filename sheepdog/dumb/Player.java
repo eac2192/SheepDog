b65ee215e50028a59501fbccac1540574b92f389
Matt Kim <mjkim823@gmail.com>
2013-11-15 17:49:19 -0500

    made original strategy baseline

--- sheepdog/dumb/Player.java
+++ sheepdog/dumb/Player.java
@@ -6,10 +6,12 @@ import sheepdog.sim.Point;
 public class Player extends sheepdog.sim.Player {
     private int nblacks;
     private boolean mode;
+    private boolean useBaseline;
 
     public void init(int nblacks, boolean mode) {
         this.nblacks = nblacks;
         this.mode = mode;
+	this.useBaseline = false;
     }
     
     // Return: the next position
@@ -51,32 +53,37 @@ public class Player extends sheepdog.sim.Player {
         	next_y=current.y+((51-current.y)/length)*2;		
         }
         else {
+	    if (useBaseline) {
         	for (int i=0; i<sheeps.length; i++){
-        		if ((i % dogs.length)==id-1 & sheeps[i].x >50 ) {
-        			length=Math.sqrt(Math.pow((current.x-sheeps[i].x),2)+Math.pow((current.y-sheeps[i].y),2));
-        			if (length>1) {
-        				System.out.printf("here it is dog %d", id);
-        				System.out.printf("we are chasing sheep %d",i);
-        				double offsetx;
-        				double offsety;
-        				double tmplength;
-        				tmplength=Math.sqrt(Math.pow((sheeps[i].x-50),2)+Math.pow((sheeps[i].y-50),2));
-        				offsetx=(sheeps[i].x-50)*(1+tmplength)/tmplength+50;
-        				offsety=(sheeps[i].y-50)*(1+tmplength)/tmplength+50;
-                	next_x=current.x+((offsetx-current.x)/length)*2;
-                	next_y=current.y+((offsety-current.y)/length)*2;
+		    if ((i % dogs.length)==id-1 & sheeps[i].x >50 ) {
+			length=Math.sqrt(Math.pow((current.x-sheeps[i].x),2)+Math.pow((current.y-sheeps[i].y),2));
+			if (length>1) {
+			    System.out.printf("here it is dog %d", id);
+			    System.out.printf("we are chasing sheep %d",i);
+			    double offsetx;
+			    double offsety;
+			    double tmplength;
+			    tmplength=Math.sqrt(Math.pow((sheeps[i].x-50),2)+Math.pow((sheeps[i].y-50),2));
+			    offsetx=(sheeps[i].x-50)*(1+tmplength)/tmplength+50;
+			    offsety=(sheeps[i].y-50)*(1+tmplength)/tmplength+50;
+			    next_x=current.x+((offsetx-current.x)/length)*2;
+			    next_y=current.y+((offsety-current.y)/length)*2;
                 	
-                	break;
+			    break;
                 	}
-        			else {
-        				length=Math.sqrt(Math.pow((current.x-50),2)+Math.pow((current.y-50),2));
-        	        	next_x=current.x+((50-current.x)/length)*2;
-        	        	next_y=current.y+((50-current.y)/length)*2;	
-        	        	break;
-        			}
+			else {
+			    length=Math.sqrt(Math.pow((current.x-50),2)+Math.pow((current.y-50),2));
+			    next_x=current.x+((50-current.x)/length)*2;
+			    next_y=current.y+((50-current.y)/length)*2;	
+			    break;
+			}
         			
-        		}
+		    }
         	}
+	    } else {
+		
+
+	    }
         }
         current.x=next_x;
         current.y=next_y;
